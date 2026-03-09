6d14d677958c8a3dfb3812fedb3fc6f3e58d962e
Logan Ryan <logan.p.ryan@icloud.com>
2026-03-09 13:29:39 -0500

    Added all homework problems from HW1 to test

--- tests/homework/test_hw1.py
+++ tests/homework/test_hw1.py
@@ -13,3 +13,47 @@ def test_problem1():
     expected = 0.0099
     
     assert_valid(actual, expected)
+
+
+def test_problem2():
+    Q_ = config.Q_
+    
+    T = Q_(936, 'rankine')
+    P = Q_(7.8, 'atm')
+    
+    actual1 = config.CP.to('ft*lbf/slug/rankine').m
+    actual2 = config.CV.to('ft*lbf/slug/rankine').m
+    actual3 = (config.CV * T).to('ft*lbf/slug').m
+    actual4 = (config.CP * T).to('ft*lbf/slug').m
+    
+    expected1 = 6008
+    expected2 = 4291
+    expected3 = 4.017e6
+    expected4 = 5.623e6
+    
+    assert_valid(actual1, expected1)
+    assert_valid(actual2, expected2)
+    assert_valid(actual3, expected3)
+    assert_valid(actual4, expected4)
+
+
+def test_problem3():
+    Q_ = config.Q_
+    
+    T = Q_(519, 'rankine')
+    P = Q_(7.8, 'atm')
+    
+    actual1 = config.CP.to('ft*lbf/slug/rankine').m
+    actual2 = config.CV.to('ft*lbf/slug/rankine').m
+    actual3 = (config.CV * T).to('ft*lbf/slug').m
+    actual4 = (config.CP * T).to('ft*lbf/slug').m
+    
+    expected1 = 6008
+    expected2 = 4291
+    expected3 = 2.227e6
+    expected4 = 3.118e6
+    
+    assert_valid(actual1, expected1)
+    assert_valid(actual2, expected2)
+    assert_valid(actual3, expected3)
+    assert_valid(actual4, expected4)
